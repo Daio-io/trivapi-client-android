8ffb114aaf5d02299dfa1a317ea9f06ae6ffa752
Daio-io <dai.williams@bbc.co.uk>
2015-11-05 00:33:59 +0000

    Create Transformer tests

---
+++ trivapiclient/src/test/java/io/daio/trivapiclient/ResponseTransformerTest.java
@@ -0,0 +1,14 @@
+package io.daio.trivapiclient;
+
+import junit.framework.TestCase;
+
+public class ResponseTransformerTest extends TestCase {
+
+    // TODO MOCK ANDROID JSON PARSING
+    public void testSuccessTransformShouldReturnTrivapiRequests() throws Exception {
+
+
+    }
+
+
+}
