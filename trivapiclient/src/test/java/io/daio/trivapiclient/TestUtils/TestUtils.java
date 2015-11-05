467310afa2effd1a322244691e412219d8cca8d0
Daio-io <dai.williams@bbc.co.uk>
2015-11-05 00:34:39 +0000

    Add TestUtils and fixtures

---
+++ trivapiclient/src/test/java/io/daio/trivapiclient/TestUtils/TestUtils.java
@@ -0,0 +1,31 @@
+package io.daio.trivapiclient.TestUtils;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TestUtils {
+
+    private static JSONParser parser = new JSONParser();
+
+    private static final String BASE_PATH = System.getProperty("user.dir") + "/src/test/java/io/daio/trivapiclient/fixtures/";
+
+    public static String loadFixture(String filename) {
+
+        try {
+            JSONObject json = (JSONObject) parser.parse(new FileReader(BASE_PATH + filename));
+            return json.toJSONString();
+
+        } catch (IOException | ParseException ignored) {
+            System.out.println(ignored.getLocalizedMessage());
+
+        }
+
+        return "";
+
+    };
+
+}
